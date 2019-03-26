ae7b640e0fce2d7f3cd59bd46287b469b4984bb0
Alok Reddy <alokreddyk@gmail.com>
2019-03-26 19:52:53 +0530

    Updates

---
+++ precache-manifest.cf8881bdeef5a48a6338f9beda843af6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c19916d06bd1b32f172",
+    "url": "/creditcard/static/js/runtime~main.48a97d2b.js"
+  },
+  {
+    "revision": "bd8484631b3f563b9532",
+    "url": "/creditcard/static/js/main.2b9d8327.chunk.js"
+  },
+  {
+    "revision": "434484860af23c5b8bf0",
+    "url": "/creditcard/static/js/2.3f1cc002.chunk.js"
+  },
+  {
+    "revision": "bd8484631b3f563b9532",
+    "url": "/creditcard/static/css/main.d3146fee.chunk.css"
+  },
+  {
+    "revision": "434484860af23c5b8bf0",
+    "url": "/creditcard/static/css/2.c2f41e2c.chunk.css"
+  },
+  {
+    "revision": "338b8e7d6acc8495c36e8a34eb6c54be",
+    "url": "/creditcard/index.html"
+  }
+];
